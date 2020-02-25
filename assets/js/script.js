c3e823e02f1e0de4c936d939cfbcac62b6f85fad
enelesmai <xochitl.selene.fp@gmail.com>
2020-02-25 15:39:11 -0600

    fixed sticky function bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-window.onscroll = function () { myFunction() };
-
 var attend = document.getElementById("attend");
-
 var sticky = attend.offsetHeight;
+var windowHeight = window.innerHeight;
+var bodyHeight = document.body.offsetHeight;
+
+window.onscroll = function () { myFunction() };
+window.onresize = function() { resizeW() };
 
 function myFunction() {
-    if ((window.innerHeight + window.scrollY) > document.body.offsetHeight-sticky) {     
+    if ((windowHeight + window.scrollY) >= bodyHeight - sticky) {     
         attend.classList.remove("fixed-bottom");
     }else{
         attend.classList.add("fixed-bottom");
     }
+}
+
+function resizeW(){
+    windowHeight = window.innerHeight;
+    bodyHeight = document.body.offsetHeight;
 }
